50f69e173d72741e317757ae9d260cd81c486416
Anthony Wilkins <awilkinsx@gmail.com>
2020-08-24 22:02:48 -0400

    testing

--- src/Application/Workouts/Queries/GenerateWorkout/GenerateWorkoutQueryHandler.cs
+++ src/Application/Workouts/Queries/GenerateWorkout/GenerateWorkoutQueryHandler.cs
@@ -40,10 +40,10 @@ public class GenerateWorkoutQueryHandler : IRequestHandler<GenerateWorkoutQuery,
             }
             Random rnd = new Random();
             var excercises = await _dbContext.ExerciseOptions.ToListAsync();
-            var ChestWorkout = excercises.Where(e => e.MuscleGroup == "Chest").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Chest").Count()) - 1];
-            var BackWorkout = excercises.Where(e => e.MuscleGroup == "Back").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Back").Count()) - 1];
-            var LegWorkout = excercises.Where(e => e.MuscleGroup == "Legs").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Legs").Count()) - 1];
-            var CoreWorkout = excercises.Where(e => e.MuscleGroup == "Core").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Core").Count()) - 1];
+            var ChestWorkout = excercises.Where(e => e.MuscleGroup == "Chest").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Chest").Count())];
+            var BackWorkout = excercises.Where(e => e.MuscleGroup == "Back").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Back").Count())];
+            var LegWorkout = excercises.Where(e => e.MuscleGroup == "Legs").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Legs").Count())];
+            var CoreWorkout = excercises.Where(e => e.MuscleGroup == "Core").ToArray()[rnd.Next(excercises.Where(e => e.MuscleGroup == "Core").Count())];
 
             var Workout = new Workout
             {
