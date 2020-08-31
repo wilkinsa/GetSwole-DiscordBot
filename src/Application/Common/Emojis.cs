b7b244e922c585d97c0a3adf19808b3c02d4e6ab
Anthony Wilkins <awilkinsx@gmail.com>
2020-08-31 09:42:27 -0400

    allow adding of water droplet emoji:

--- src/Application/Common/Emojis.cs
+++ src/Application/Common/Emojis.cs
@@ -3,5 +3,6 @@ namespace Application.Common
     public static class Emojis
     {
         public static string white_check_mark = "\u2705";
+        public static string droplet = "\uD83D\uDCA7";
     }
 }
